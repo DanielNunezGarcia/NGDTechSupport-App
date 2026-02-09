1e94e22345f62064d2c8d418dc6cd2204fad2afe
DanielNunezGarcia <danielnunezgarcia1995@gmail.com>
2026-02-09 19:34:04 +0100

    Fix resource imports and activity packages

--- app/src/main/java/com/example/ngdtechsupport/ui/updates/UpdatesActivity.kt
+++ app/src/main/java/com/example/ngdtechsupport/ui/updates/UpdatesActivity.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ngdtechsupport.R
 
-
 class UpdatesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_updates)
+        setContentView(R.layout.activity_chat)
     }
 }
