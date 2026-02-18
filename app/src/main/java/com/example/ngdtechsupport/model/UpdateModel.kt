cfeafea8082d91740ebc8baffa8aed647049fcc1
DanielNunezGarcia <danielnunezgarcia1995@gmail.com>
2026-02-18 17:50:21 +0100

    FASE 5 - Bloque 2: Creado UpdateModel alineado con Firestore

--- app/src/main/java/com/example/ngdtechsupport/model/UpdateModel.kt
+++ app/src/main/java/com/example/ngdtechsupport/model/UpdateModel.kt
@@ -1,9 +1,12 @@
 package com.example.ngdtechsupport.model
 
+import com.google.firebase.Timestamp
+
 data class UpdateModel(
     val id: String = "",
-    val appId: String = "",
     val title: String = "",
     val description: String = "",
-    val date: String = ""
+    val type: String = "",
+    val createdAt: Timestamp? = null,
+    val createdBy: String = ""
 )
