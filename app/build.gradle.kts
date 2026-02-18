76aa0ae3258c07e1fcd66cc2acc139937bb78ec0
DanielNunezGarcia <danielnunezgarcia1995@gmail.com>
2026-02-18 18:51:12 +0100

    FASE 5 - Bloque 6: Creado layout PublishUpdateActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,8 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-firestore-ktx")
-    
+    implementation(libs.androidx.activity)
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
