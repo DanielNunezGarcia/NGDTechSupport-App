802fe44b16ae3bafc85520600075a4d22db66725
DanielNunezGarcia <danielnunezgarcia1995@gmail.com>
2026-02-19 18:17:36 +0100

    FASE 6 - Bloque 2: Creado ChatMessageModel

---
+++ app/src/main/java/com/example/ngdtechsupport/model/ChatMessageModel.kt
@@ -0,0 +1,11 @@
+package com.example.ngdtechsupport.data.model
+
+import com.google.firebase.Timestamp
+
+data class ChatMessageModel(
+    val id: String = "",
+    val message: String = "",
+    val senderId: String = "",
+    val senderRole: String = "",
+    val createdAt: Timestamp? = null
+)
